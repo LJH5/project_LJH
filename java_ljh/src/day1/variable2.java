00112fc0592358b803b7ffc715297479f9ce7152
LJH5 <sigma6317@gmail.com>
2021-04-26 17:35:30 +0900

    자바1일차 : 변수면 작성 규칙 예제

---
+++ java_ljh/src/day1/variable2.java
@@ -0,0 +1,28 @@
+package day1;
+
+public class variable2 {
+
+	public static void main(String[] args) {
+		int num1 = 1;
+		//int num1 = 2; //에러 발생 : 중복 선언
+		
+		//int 1num = 1; //에러 발생 : 숫자로 시작할 수 없음\
+		
+		int num$ = 1;
+		int _num = 1;
+		//int num% = 1; //에러 발생 : '$'와 '_'가 아닌 문자 사용
+		
+		//int public = 1; //에러 발생 : 예약어 사용
+		
+		/*변수명 작성 관례(필수x)
+		 * 의미있는 단어로 구성
+		 * 카멜 표기법 : 두 단어로 구성된 경우, 두번째 단어부터 첫글자만 대문자로 사용
+		 */
+		//학생수를 저장하기 위한 변수를 생성해야 한다.
+		int a = 5;
+		int studentCount = 5;
+		
+		
+	}
+
+}
