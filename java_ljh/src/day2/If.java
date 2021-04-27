bba7601ca2903ee2bd81434dbeeb9206c4e2364f
LJH5 <sigma6317@gmail.com>
2021-04-27 16:48:12 +0900

    자바2일차: If문 예제

---
+++ java_ljh/src/day2/If.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class If {
+
+	public static void main(String[] args) {
+		// If문 예제 1
+		
+		int num = 1;
+		
+		if(num == 0){
+			System.out.println("0입니다.");
+		} else if(num == 1) {
+			System.out.println("1입니다.");		
+		} else {
+			System.out.println("0과 1이 아닙니다.");
+		}
+
+	}		
+
+}
