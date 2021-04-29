1ae05b57099d52d6986261a838eb5cf00188cdc3
LJH5 <sigma6317@gmail.com>
2021-04-29 17:14:12 +0900

    자바4일차: Continue 예제

--- java_ljh/src/day4/Continue.java
+++ java_ljh/src/day4/Continue.java
@@ -5,7 +5,7 @@ public class Continue {
 	public static void main(String[] args) {
 		// continue: 스킵, 특정 위치로 이동, for문에서는 증감식 위치로, while문 은 조건식으로 이동, 항상 if문과 사용
 		int i;
-		for(i=1; i<=5; i+=1) {
+		for(i=1; i<=5; i += 1) {
 			if(i == 2) {
 				continue;	
 			}
