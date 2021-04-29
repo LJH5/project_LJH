32193db3c11e4d885b060b899dc2cddeb3bcc665
LJH5 <sigma6317@gmail.com>
2021-04-29 13:00:14 +0900

    자바4일차: For문 무한루프

---
+++ java_ljh/src/day4/For9.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class For9 {
+
+	public static void main(String[] args) {
+		// 무한루프 : 반복문이 끝나지 않는 경우를 무한루프라 한다.
+		int i = 1;
+		for( ; i < 5 ; ) {
+			System.out.println("Hello would!");
+		}
+		
+		// 한 번도 실행되지 않음
+		for( ; i > 5 ; ) {
+			System.out.println("Hello would!");
+		} 		
+	}
+}
