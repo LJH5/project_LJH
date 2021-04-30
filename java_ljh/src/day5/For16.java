49de63bffe5fc0022c534af2c7df70508c39f06b
LJH5 <sigma6317@gmail.com>
2021-04-30 10:19:56 +0900

    자바5일차: For문 *출력 응용

---
+++ java_ljh/src/day5/For16.java
@@ -0,0 +1,38 @@
+package day5;
+
+public class For16 {
+	
+	public static void main(String[] args) {
+		// 중첩 for문을 이용해 *출력
+		int k, i;
+		for(k=1; k<=5; k += 1) {
+			for(i=1; i<=k; i += 1) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println("--------");
+		
+		for(k=1; k<=5; k += 1) {
+			for(i=1; i<=4; i += 1) {
+				System.out.print(" ");
+			}
+			for(i=1; i<=4; i += 1) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println("--------");
+		
+		for(k=1; k<=5; k += 1) {
+			for(i=1; i<=5-k; i += 1) {
+				System.out.print(" ");
+			}
+			for(i=1; i<=k; i += 1) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+}  
