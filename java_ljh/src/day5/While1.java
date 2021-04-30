6be6bbd340eeeb796ab36c781e840d09ed0de654
LJH5 <sigma6317@gmail.com>
2021-04-30 13:49:40 +0900

    자바5일차: While 1~5까지 출력

---
+++ java_ljh/src/day5/While1.java
@@ -0,0 +1,16 @@
+package day5;
+
+public class While1 {
+
+	public static void main(String[] args) {
+		// 1~5까지 While 문을 이용하여 출력
+		int i = 1;
+		
+		while(i<=5) {
+			System.out.print(i + " ");
+			i += 1;
+		}
+			
+
+	}
+}
