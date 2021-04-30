fd6d3a038d5a9ee5c3c88c3cce8fc8161e785dff
LJH5 <sigma6317@gmail.com>
2021-04-30 10:41:01 +0900

    자바5일차: For문 *출력 응용

--- java_ljh/src/day5/For16.java
+++ java_ljh/src/day5/For16.java
@@ -11,7 +11,7 @@ public class For16 {
 			}
 			System.out.println();
 		}
-		System.out.println("--------");
+		System.out.println("---------");
 		
 		for(k=1; k<=5; k += 1) {
 			for(i=1; i<=4; i += 1) {
@@ -22,7 +22,7 @@ public class For16 {
 			}
 			System.out.println();
 		}
-		System.out.println("--------");
+		System.out.println("---------");
 		
 		for(k=1; k<=5; k += 1) {
 			for(i=1; i<=5-k; i += 1) {
@@ -33,6 +33,49 @@ public class For16 {
 			}
 			System.out.println();
 		}
+		System.out.println("---------");
 		
+		
+		
+		
+		
+		/*          *
+		 *         ***
+		 *        *****
+		 *       *******
+		 *      *********  
+		 *      
+		 *      *********
+		 *       *******
+		 *        *****
+		 *         ***
+		 *          *	  
+		 */    
+		for(k=1; k<=5; k += 1) {
+			for(i=1; i<=5-k; i += 1) {
+				System.out.print(" ");
+			}
+			for(i=1; i<=k; i += 1) {
+				System.out.print("*");
+			}
+			for(i=1; i<=k-1; i +=1 ) {
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println("---------");
+		
+		for(k=1; k<=5; k += 1) {
+			for(i=1; i<=k-1; i += 1) {
+				System.out.print(" ");
+			}
+			for(i=1; i<=6-k; i += 1) {
+				System.out.print("*");
+			}
+			for(i=1; i<=5-k; i +=1 ) {
+				System.out.print("*");
+				}
+			System.out.println();
+		}
 	}
 }  
