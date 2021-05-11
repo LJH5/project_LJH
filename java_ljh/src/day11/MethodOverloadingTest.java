8ab13eb19d71620dadccf47e99daf9f745310a35
leejh <sigma6317@gmail.com>
2021-05-11 13:04:17 +0900

    자바11일차: 메소드 오버로딩

---
+++ java_ljh/src/day11/MethodOverloadingTest.java
@@ -0,0 +1,52 @@
+package day11;
+
+public class MethodOverloadingTest {
+
+	public static void main(String[] args) {
+		Point2 pt = new Point2();
+		pt.print();
+		pt.move(4,5);
+		pt.print();
+		
+		Point2 pt2 = new Point2(10,20);
+		pt.move(pt2);
+		pt.print();
+		
+		System.out.println(1);
+		System.out.println("1");
+		System.out.println('1');
+
+	}
+
+}
+
+class Point2{
+	private int x, y;
+	public Point2() {}
+	public Point2(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+	public void print() {
+		System.out.println("(" + x + ", " + y + ")");
+	}
+	/* 기능	 : 이동할 x,y 좌표가 주어지면 해당 좌표로 이동하는 메소드
+	 * 매개변수: int x, int y
+	 * 리턴타입: void
+	 * 메소드명: move
+	 */
+	public void move(int x, int y) {
+		this.x=x;
+		this.y=y;
+	}
+	
+	/* 기능	 : 이동할 좌표가 Point2의 객체로 주어지면 해당 좌표로 이동하는 메소드
+	 * 매개변수: point2 pt
+	 * 리턴타입: void
+	 * 메소드명: move
+	 */
+	public void move(Point2 pt) {
+		x=pt.x;
+		y=pt.y;
+	}
+}
