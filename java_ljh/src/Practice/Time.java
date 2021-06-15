9700f99ae4fc40141f4ae8adf5210d1e0cb3ba7b
LJH5 <sigma6317@gmail.com>
2021-06-15 09:41:18 +0900

    연습

---
+++ java_ljh/src/Practice/Time.java
@@ -0,0 +1,40 @@
+package Practice;
+
+public class Time {
+	
+	private int hour;
+	private int minute;
+	private float second;
+	
+	
+	public int getHour() {
+		return hour;
+	}
+	public void setHour(int hour) {
+		if(hour<0 || hour>23) {
+			return;
+		}
+		this.hour = hour;
+	}
+	public int getMinute() {
+		return minute;
+	}
+	public void setMinute(int minute) {
+		if(minute<0 || minute>59) {
+			return;
+		}
+		this.minute = minute;
+	}
+	public float getSecond() {
+		return second;
+	}
+	public void setSecond(float second) {
+		if(second<0 || second>=60) {
+			return;
+		}
+		this.second = second;
+	}
+	
+
+	
+}
