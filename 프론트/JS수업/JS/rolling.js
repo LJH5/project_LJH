c43403251147e3319849a7edb4aaead2c6e54e3d
leejh <83192174+ppp294@users.noreply.github.com>
2021-06-25 16:21:57 +0900

    Update rolling.js

--- 프론트/JS수업/JS/rolling.js
+++ 프론트/JS수업/JS/rolling.js
@@ -11,12 +11,35 @@ function rollingTop(list, listItem, marginTop, animateTime, intervalTime){
 
 function rollingLeft(list, listItem, animateTime, intervalTime){
 	var id = setInterval(function(){
-		if($(list +' ' + listItem).first().is(':animated')){
+		if(!$(list +' ' + listItem).first().is(':animated')){
 			var width = $(list +' ' + listItem).first().width();
-			$(list +' ' + listItem).first().animate({'margin-left':'-'+width}, animateTime,function(){
+			$(list +' ' + listItem).first().animate({'margin-left':'-'+width},animateTime,function(){
 				$(this).detach().appendTo(list).removeAttr('style');
 			})
 		}
-	},1500);
+	},intervalTime);
 	return id;
+}
+
+function rollingRight(list, listItem, animateTime, intervalTime){
+	var id = setInterval(function(){
+		if(!$(list +' ' + listItem).first().is(':animated')){
+			var width = $(list +' ' + listItem).last().width();
+			$(list +' ' + listItem).last().detach().prependTo(list).css('margin-left',-width+'px').animate({'margin-left':'0px'},animateTime);
+		}
+	},intervalTime);
+	return id;
+}
+
+function rolling(direction, list, listItem, margin, animateTime, intervalTime){
+	if(direction.toLowerCase()=='left'){
+		return rollingLeft(list, listItem, animateTime, intervalTime);
+	}
+	else if(direction.toLowerCase()=='right'){
+		return rollingRight(list, listItem, animateTime, intervalTime);
+	}
+	else{
+		return rollingTop(list, listItem, margin, animateTime, intervalTime);
+	}
+
 }
