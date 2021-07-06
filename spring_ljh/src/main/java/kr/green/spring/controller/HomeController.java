f97dd9b953396088cc576863252fb77e452eb135
leejh <83192174+ppp294@users.noreply.github.com>
2021-07-06 17:03:25 +0900

    signup

--- spring_ljh/src/main/java/kr/green/spring/controller/HomeController.java
+++ spring_ljh/src/main/java/kr/green/spring/controller/HomeController.java
@@ -29,11 +29,31 @@ public class HomeController {
 		System.out.println(user);
 		// 서비스에게  user의 아이디와 비밀번호를 전달하면 해당 정보가 있는지 없는지 판단 후 DB에 있으면 해당 정보를 없으면 null을 반환
 		MemberVO dbUser = memberService.signin(user);
+		// 회원정보가 있으면
 		if(dbUser != null) {
 			// 작업이 다 끝난 후 URI가 /signin이 있는 곳으로 넘어감
 			mv.setViewName("redirect:/");
+		// 회원 정보가 없으면 
 		}else {
 			mv.setViewName("redirect:/signin");
+		}
+		return mv;
+	}
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public ModelAndView signupGet(ModelAndView mv) {
+		mv.setViewName("signup");
+		return mv;
+	}
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public ModelAndView signupPost(ModelAndView mv, MemberVO user) {
+		System.out.println(user);
+		// 서비스에게 아이디를 주면서 회원 가입을 하라고 일을 시키고, 회원 가입이 성공하면 true 아니면 false
+		boolean isSignup = memberService.signup(user);
+		// 성공하면 메인, 아니면 가입창
+		if(isSignup) {
+			mv.setViewName("redirect:/");			
+		}else {
+			mv.setViewName("redirect:/signup");
 		}
 		return mv;
 	}
