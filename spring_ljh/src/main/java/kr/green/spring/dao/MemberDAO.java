d7a96e489b3d2a9ba28a3d8f0f75de8bf8059140
LJH5 <sigma6317@gmail.com>
2021-07-07 21:15:59 +0900

    delete

--- spring_ljh/src/main/java/kr/green/spring/dao/MemberDAO.java
+++
@@ -1,9 +0,0 @@
-package kr.green.spring.dao;
-
-import org.apache.ibatis.annotations.Param;
-
-import kr.green.spring.vo.MemberVO;
-
-public interface MemberDAO {
-	public MemberVO getMember(@Param("id")String id);
-}
--- spring_ljh/src/main/java/kr/green/spring/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package kr.green.spring.service;
-
-import kr.green.spring.vo.MemberVO;
-
-public interface MemberService {
-	public MemberVO getMember(String id);
-}
