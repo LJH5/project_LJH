5dcd21cb11c2490569ee7edfd58e190932aecaa0
LJH5 <sigma6317@gmail.com>
2021-07-23 15:21:05 +0900

    Update ReplyController.java

--- springtest/src/main/java/kr/green/test/controller/ReplyController.java
+++ springtest/src/main/java/kr/green/test/controller/ReplyController.java
@@ -1,9 +1,18 @@
 package kr.green.test.controller;
 
-import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-import kr.green.test.service.*;
-import kr.green.test.vo.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.green.test.pagination.Criteria;
+import kr.green.test.service.ReplyService;
+import kr.green.test.vo.ReplyVO;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -16,5 +25,16 @@ public class ReplyController {
 	public String replyInsGet(@RequestBody ReplyVO rvo) {
 		return replyService.insertReply(rvo) == 0 ? "FAIL" : "OK";
 	}
-
+	@GetMapping("/reply/list/{rp_bd_num}/{page}")
+	public Map<String, Object> replyListGet(@PathVariable("rp_bd_num") int rp_bd_num, @PathVariable("page") int page){
+		//System.out.println(rp_bd_num);
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		//System.out.println(page);
+		Criteria cri = new Criteria(page, 3);
+		//3은 한 페이지당 댓글 수
+		ArrayList<ReplyVO> list = replyService.getReplyList(rp_bd_num, cri);
+		//System.out.println(list); 
+		map.put("replyList", list); //xml로 온다
+		return map;
+	}
 }
