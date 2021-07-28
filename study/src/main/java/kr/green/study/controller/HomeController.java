0d783f16bbd48e07fda768a08bb658ead6067552
leejh <83192174+ppp294@users.noreply.github.com>
2021-07-28 11:05:22 +0900

    controller 세팅

--- study/src/main/java/kr/green/study/controller/HomeController.java
+++ study/src/main/java/kr/green/study/controller/HomeController.java
@@ -4,13 +4,11 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import kr.green.study.service.MemberService;
 import lombok.AllArgsConstructor;
@@ -21,17 +19,12 @@ public class HomeController {
 
 	MemberService memberService;
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	@GetMapping("/")
+	public ModelAndView home(ModelAndView mv) {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "/template/main/home";
+		mv.addObject("title", "메인");
+		mv.setViewName("/template/main/home");
+		return mv;
 	}
 	
 }
